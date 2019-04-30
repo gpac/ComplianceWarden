50455507dedcdf673e7fd49482fccb106569132a
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2019-04-30 10:29:36 +0200

    Document interfaces

--- src/spec.h
+++ src/spec.h
@@ -1,3 +1,4 @@
+// This is how the app sees concrete specifications.
 #pragma once
 
 #include <vector>
@@ -5,19 +6,29 @@
 
 struct IReport
 {
+  // add a human-readable error to the report.
   virtual void error(const char* fmt, ...) = 0;
 };
 
 struct RuleDesc
 {
+  // human-readable description of the rule
   const char* caption;
+
+  // apply this rule to the file 'root',
+  // will push the results (messages) to the 'out' report.
   void (* check)(Box const& root, IReport* out);
 };
 
 struct SpecDesc
 {
+  // short name for the spec (used for command-line spec selection).
   const char* name;
+
+  // human-readable description of the spec (name, version, date, etc.).
   const char* caption;
+
+  // list of compliance checks for this spec.
   std::vector<RuleDesc> rules;
 };
 
