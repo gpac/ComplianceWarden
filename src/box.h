af99a640cf509ee3ae205cf982fd6f62276f621a
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-06-10 08:39:39 +0200

    c++14-compatible static assert

--- src/box.h
+++ src/box.h
@@ -19,7 +19,7 @@ struct Box
 template<size_t N>
 constexpr uint32_t FOURCC(const char(&tab)[N])
 {
-  static_assert(N == 4 + 1);
+  static_assert(N == 4 + 1, "FOURCC input size must be 4 + 1");
   uint32_t r = 0;
 
   for(int i = 0; i < 4; ++i)
