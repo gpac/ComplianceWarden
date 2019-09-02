668ed7ea544c4af40346a4aa88e3b7586d68063b
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-09-02 16:30:22 +0200

    miaf: test data-reference-index value

---
+++ tests/miaf/invalid-data-reference-index.asm
@@ -0,0 +1,74 @@
+%define BE(a) ( ((((a)>>24)&0xFF) << 0) + ((((a)>>16)&0xFF) << 8) + ((((a)>>8)&0xFF) << 16)  + ((((a)>>0)&0xFF) << 24) )
+
+ftyp_start:
+dd BE(ftyp_end - ftyp_start)
+db "ftyp"
+
+db "isom"
+dd BE(0x200)
+db "mif1", "miaf"
+
+ftyp_end:
+
+meta_start:
+dd BE(meta_end - meta_start)
+db "meta"
+dd BE(0)
+
+hdlr_start:
+dd BE(hdlr_end - hdlr_start)
+db "hdlr"
+dd BE(0)
+dd BE(0)
+db "pict"
+dd BE(0)
+dd BE(0)
+dd BE(0)
+hdlr_end:
+
+pitm_start:
+dd BE(pitm_end - pitm_start)
+db "pitm"
+dd BE(0)
+db 0xaa, 0xbb
+pitm_end:
+
+iinf_start:
+dd BE(iinf_end - iinf_start)
+db "iinf"
+dd BE(0)
+db 0x00, 0x00
+iinf_end:
+
+iloc_start:
+dd BE(iloc_end - iloc_start)
+db "iloc"
+dd BE(0x01000000)
+dd BE(1) ; 1 item
+dd BE(0) ; construction_method
+db 0, 1 ; invalid data_reference_index
+dw BE(0)
+iloc_end:
+
+iref_start:
+dd BE(iref_end - iref_start)
+db "iref"
+dd BE(0)
+iref_end:
+
+iprp_start:
+dd BE(iprp_end - iprp_start)
+db "iprp"
+ipco_start:
+dd BE(ipco_end - ipco_start)
+db "ipco"
+ispe_start:
+dd BE(ispe_end - ispe_start)
+db "ispe"
+ispe_end:
+ipco_end:
+iprp_end:
+
+meta_end:
+
+; vim: syntax=nasm
