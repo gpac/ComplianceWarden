a910e2727f57396970cfd1ef625c899077a83b19
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-06-08 15:24:56 +0200

    add avif structure

---
+++ src/spec_avif.cpp
@@ -0,0 +1,23 @@
+#include "spec.h"
+#include "fourcc.h"
+
+static const SpecDesc spec =
+{
+  "avif",
+  "AVIF v1.0.0, 19 February 2019\n"
+  "https://aomediacodec.github.io/av1-avif/",
+  {
+    {
+      "'ftyp' box must appear first",
+      [] (Box const& root, IReport* out)
+      {
+        if(root.children.empty() || root.children[0].fourcc != FOURCC("ftyp"))
+          out->error("first box must be ftyp");
+      }
+    },
+  },
+  nullptr,
+};
+
+static auto const registered = registerSpec(&spec);
+
