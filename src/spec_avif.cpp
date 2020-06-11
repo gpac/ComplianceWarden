45d9c034250e4b0ca9ffbc95dbd970a2bef7d3fc
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-06-11 14:51:22 +0200

    avif: add empty functions for upcoming rules

--- src/spec_avif.cpp
+++ src/spec_avif.cpp
@@ -4,6 +4,16 @@
 #include <cstring> // strcmp
 #include <map>
 
+#if 0
+namespace
+{
+void parse_seq_header()
+{
+  // TODO
+}
+}
+#endif
+
 extern void checkEssential(Box const& root, IReport* out, uint32_t fourcc);
 
 static const SpecDesc spec =
@@ -76,7 +86,94 @@ static const SpecDesc spec =
           if(item.second == false)
             out->error("AV1 Image Item (ID=%u) shall be associated with an AV1 Item Configuration Property", item.first);
       }
-    }
+    },
+    {
+      "Section 2.1\n"
+      "The AV1 Image Item Data shall be identical to the content of an AV1 Sample marked as sync",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.1\n"
+      "The AV1 Image Item Data shall have exactly one Sequence Header OBU.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.1\n"
+      "The AV1 Image Item Data should have its still_picture flag set to 1.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.1\n"
+      "The AV1 Image Item Data should have its reduced_still_picture_header flag set to 1.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.2.1\n"
+      "If a Sequence Header OBU is present in the AV1CodecConfigurationBox, it shall match the\n"
+      "Sequence Header OBU in the AV1 Image Item Data.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.2.1\n"
+      "The values of the fields in the AV1CodecConfigurationBox shall match those of the\n"
+      "Sequence Header OBU in the AV1 Image Item Data.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.2.1\n"
+      "Metadata OBUs, if present, shall match the values given in other item properties, such as\n"
+      "the PixelInformationProperty or ColourInformationBox.",
+      [] (Box const& root, IReport* out)
+      {
+        (void)root;
+        (void)out; // TODO
+      }
+    },
+    {
+      "Section 2.2.1\n"
+      "AV1 Item Configuration Property [...] shall be marked as essential.",
+      [] (Box const& root, IReport* out)
+      {
+        checkEssential(root, out, FOURCC("av1C"));
+      }
+    },
+#if 0
+    {
+      "Section 4. Auxiliary Image Items\n"
+      "The mono_chrome field in the Sequence Header OBU shall be set to 1.",
+      [] (Box const& root, IReport* out)
+      {
+        // TODO: is an AV1 Image Item OR  AV1 Image Sequence => Check against 2.1
+        // TODO: check Auxiliary
+
+        // TODO: parse seq hdr until color config
+      }
+    },
+#endif
   },
   nullptr,
 };
