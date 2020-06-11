5774653635c087d8f711af5affd494ac6efc727f
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-06-11 14:45:29 +0200

    avif: section 2.1 first shall

--- src/spec_avif.cpp
+++ src/spec_avif.cpp
@@ -1,5 +1,8 @@
 #include "spec.h"
 #include "fourcc.h"
+#include <algorithm> // std::find
+#include <cstring> // strcmp
+#include <map>
 
 extern void checkEssential(Box const& root, IReport* out, uint32_t fourcc);
 
@@ -11,13 +14,69 @@ static const SpecDesc spec =
   { "heif" },
   {
     {
-      "Section 2.2.1\n"
-      "AV1 Item Configuration Property [...] shall be marked as essential.",
+      "Section 2.1 AV1 Image Item\n"
+      "The AV1 Image Item shall be associated with an AV1 Item Configuration Property.",
       [] (Box const& root, IReport* out)
       {
-        checkEssential(root, out, FOURCC("av1C"));
+        std::map<uint32_t /*itemId*/, bool> av1ImageItemIDs;
+
+        // Find AV1 Image Items
+        for(auto& box : root.children)
+          if(box.fourcc == FOURCC("meta"))
+            for(auto& metaChild : box.children)
+              if(metaChild.fourcc == FOURCC("iinf"))
+                for(auto& iinfChild : metaChild.children)
+                  if(iinfChild.fourcc == FOURCC("infe"))
+                  {
+                    uint32_t itemId = 0;
+
+                    for(auto& sym : iinfChild.syms)
+                    {
+                      if(!strcmp(sym.name, "item_ID"))
+                        itemId = sym.value;
+                      else if(!strcmp(sym.name, "item_type"))
+                        if(sym.value == FOURCC("av01"))
+                          av1ImageItemIDs.insert({ itemId, false });
+                    }
+                  }
+
+        // AV1 Image Item shall be associated with an AV1 Item Configuration Property
+        std::vector<uint32_t> av1cPropertyIndex; /* 1-based */
+
+        for(auto& box : root.children)
+          if(box.fourcc == FOURCC("meta"))
+            for(auto& metaChild : box.children)
+              if(metaChild.fourcc == FOURCC("iprp"))
+                for(auto& iprpChild : metaChild.children)
+                  if(iprpChild.fourcc == FOURCC("ipco"))
+                    for(uint32_t i = 1; i <= iprpChild.children.size(); ++i)
+                      if(iprpChild.children[i - 1].fourcc == FOURCC("av1C"))
+                        av1cPropertyIndex.push_back(i);
+
+        for(auto& box : root.children)
+          if(box.fourcc == FOURCC("meta"))
+            for(auto& metaChild : box.children)
+              if(metaChild.fourcc == FOURCC("iprp"))
+                for(auto& iprpChild : metaChild.children)
+                  if(iprpChild.fourcc == FOURCC("ipma"))
+                  {
+                    uint32_t itemId = 0;
+
+                    for(auto& sym : iprpChild.syms)
+                    {
+                      if(!strcmp(sym.name, "item_ID"))
+                        itemId = sym.value;
+                      else if(!strcmp(sym.name, "property_index"))
+                        if(std::find(av1cPropertyIndex.begin(), av1cPropertyIndex.end(), sym.value) != av1cPropertyIndex.end())
+                          av1ImageItemIDs[itemId] = true;
+                    }
+                  }
+
+        for(auto& item : av1ImageItemIDs)
+          if(item.second == false)
+            out->error("AV1 Image Item (ID=%u) shall be associated with an AV1 Item Configuration Property", item.first);
       }
-    },
+    }
   },
   nullptr,
 };
