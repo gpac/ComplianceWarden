92529fcf82bfaa2af2107656ed43e3e2568e8df5
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-09-08 22:48:33 +0200

    avif: The values of the fields in the AV1CodecConfigurationBox shall match those of the Sequence Header OBU in the AV1 Image Item Data

--- src/spec_avif.cpp
+++ src/spec_avif.cpp
@@ -8,17 +8,210 @@
 #include <stdexcept>
 
 void checkEssential(Box const& root, IReport* out, uint32_t fourcc);
+std::vector<const Box*> findBoxes(const Box& root, uint32_t fourcc);
 
 namespace
 {
-int parseAv1SeqHdr(IReader* br, bool& reduced_still_picture_header)
+struct AV1CodecConfigurationRecord
 {
-  br->sym("seq_profile", 3);
+  int64_t seq_profile;
+  int64_t seq_level_idx_0;
+  int64_t seq_tier_0;
+  int64_t high_bitdepth;
+  int64_t twelve_bit;
+  int64_t mono_chrome;
+  int64_t chroma_subsampling_x;
+  int64_t chroma_subsampling_y;
+  int64_t chroma_sample_position;
+  // initial_presentation_delay
+  // configOBUs[]
+};
+
+#define READ_UNTIL_NEXT_BYTE(readBits) \
+  if(readBits % 8){ \
+    auto remainderBits = 8 - (readBits % 8); \
+    br->sym("bits", remainderBits); \
+    readBits += remainderBits; \
+  }
+
+#define AV1_COLOR_CFG_BITS 1 + (seq_profile == 2 && av1c.high_bitdepth) + !(seq_profile == 1) + 1 + color_description_present_flag * 24 + \
+  (av1c.mono_chrome ? 1 : ( \
+     !(color_primaries == 0 /*CP_BT_709*/ && transfer_characteristics == 13 /*TC_SRGB*/ && matrix_coefficients == 0) ? 1 + \
+     ((seq_profile != 0 && seq_profile != 1 && BitDepth == 12) * (1 + av1c.chroma_subsampling_x) \
+      + (av1c.chroma_subsampling_x && av1c.chroma_subsampling_y) * 2 + 1) : 0 \
+     ) \
+  )
+
+int parseAv1ColorConfig(IReader* br, int64_t seq_profile, AV1CodecConfigurationRecord& av1c)
+{
+  av1c.high_bitdepth = br->sym("high_bitdepth", 1);
+  int BitDepth = 8;
+
+  if(seq_profile == 2 && av1c.high_bitdepth)
+  {
+    av1c.twelve_bit = br->sym("twelve_bit", 1);
+    BitDepth = av1c.twelve_bit ? 12 : 10;
+  }
+  else if(seq_profile <= 2)
+  {
+    BitDepth = av1c.high_bitdepth ? 10 : 8;
+  }
+
+  if(seq_profile == 1)
+  {
+    // av1c.mono_chrome = br->sym("mono_chrome", 0); // = 0;
+  }
+  else
+  {
+    av1c.mono_chrome = br->sym("mono_chrome", 1);
+  }
+
+  // NumPlanes = mono_chrome ? 1 : 3;
+  auto color_description_present_flag = br->sym("color_description_present_flag", 1);
+  uint8_t color_primaries, transfer_characteristics, matrix_coefficients;
+
+  if(color_description_present_flag)
+  {
+    color_primaries = br->sym("color_primaries", 8);
+    transfer_characteristics = br->sym("transfer_characteristics", 8);
+    matrix_coefficients = br->sym("matrix_coefficients", 8);
+  }
+  else
+  {
+    color_primaries = 2;// CP_UNSPECIFIED;
+    transfer_characteristics = 2;// TC_UNSPECIFIED;
+    matrix_coefficients = 2;// MC_UNSPECIFIED;
+  }
+
+  if(av1c.mono_chrome)
+  {
+    br->sym("color_range", 1);
+    av1c.chroma_subsampling_x = 1;
+    av1c.chroma_subsampling_y = 1;
+    av1c.chroma_sample_position = 0;// CSP_UNKNOWN;
+    br->sym("separate_uv_delta_q", 0); // = 0;
+    auto readBits = AV1_COLOR_CFG_BITS;
+    READ_UNTIL_NEXT_BYTE(readBits);
+    return readBits;
+  }
+  else if(color_primaries == 0 /*CP_BT_709*/ &&
+          transfer_characteristics == 13 /*TC_SRGB*/ &&
+          matrix_coefficients == 0 /*MC_IDENTITY*/)
+  {
+    // color_range = 1;
+    av1c.chroma_subsampling_x = 0;
+    av1c.chroma_subsampling_y = 0;
+  }
+  else
+  {
+    br->sym("color_range", 1);
+
+    if(seq_profile == 0)
+    {
+      av1c.chroma_subsampling_x = 1;
+      av1c.chroma_subsampling_y = 1;
+    }
+    else if(seq_profile == 1)
+    {
+      av1c.chroma_subsampling_x = 0;
+      av1c.chroma_subsampling_y = 0;
+    }
+    else
+    {
+      if(BitDepth == 12)
+      {
+        av1c.chroma_subsampling_x = br->sym("subsampling_x", 1);
+
+        if(av1c.chroma_subsampling_x)
+          av1c.chroma_subsampling_y = br->sym("subsampling_y", 1);
+        else
+          av1c.chroma_subsampling_y = 0;
+      }
+      else
+      {
+        av1c.chroma_subsampling_x = 1;
+        av1c.chroma_subsampling_y = 0;
+      }
+    }
+
+    if(av1c.chroma_subsampling_x && av1c.chroma_subsampling_y)
+    {
+      br->sym("chroma_sample_position", 1);
+    }
+  }
+
+  br->sym("separate_uv_delta_q", 1);
+
+  auto readBits = AV1_COLOR_CFG_BITS;
+  READ_UNTIL_NEXT_BYTE(readBits);
+  return readBits;
+}
+
+int parseAv1SeqHdr(IReader* br, bool& reduced_still_picture_header, AV1CodecConfigurationRecord& av1c)
+{
+  av1c.seq_profile = br->sym("seq_profile", 3);
   br->sym("still_picture", 1);
   reduced_still_picture_header = br->sym("reduced_still_picture_header", 1);
-  br->sym("", 3);
-  // incomplete parsing
-  return 1;
+
+  if(reduced_still_picture_header)
+  {
+    br->sym("timing_info_present_flag", 0); // =0
+    br->sym("decoder_model_info_present_flag", 0); // =0
+    br->sym("initial_display_delay_present_flag", 0); // =0
+    br->sym("operating_points_cnt_minus_1", 0); // =0
+    br->sym("operating_point_idc_0", 0); // =0
+    av1c.seq_level_idx_0 = br->sym("seq_level_idx_0", 5);
+    av1c.seq_tier_0 = br->sym("seq_tier_0", 0); // =0
+    br->sym("decoder_model_present_for_this_op_0", 0); // =0
+    br->sym("initial_display_delay_present_for_this_op_0", 0); // =0
+  }
+  else
+  {
+    fprintf(stderr, "AV1 Sequence Header parsing with reduced_still_picture_header=0 not implemented.");
+    br->sym("", 3);
+    // incomplete parsing
+    return 1;
+  }
+
+  auto frame_width_bits_minus_1 = br->sym("frame_width_bits_minus_1", 4);
+  auto frame_height_bits_minus_1 = br->sym("frame_height_bits_minus_1", 4);
+  br->sym("max_frame_width_minus_1", frame_width_bits_minus_1 + 1);
+  br->sym("max_frame_height_minus_1", frame_height_bits_minus_1 + 1);
+  // if ( reduced_still_picture_header )
+  // frame_id_numbers_present_flag = 0
+  // else
+  // frame_id_numbers_present_flag f(1)
+  // if ( frame_id_numbers_present_flag ) {
+  // delta_frame_id_length_minus_2 f(4)
+  // additional_frame_id_length_minus_1 f(3)
+  // }
+  br->sym("use_128x128_superblock", 1);
+  br->sym("enable_filter_intra", 1);
+  br->sym("enable_intra_edge_filter", 1);
+
+  if(reduced_still_picture_header)
+  {
+    br->sym("enable_interintra_compound", 0); // =0
+    br->sym("enable_masked_compound", 0); // =0
+    br->sym("enable_warped_motion", 0); // =0
+    br->sym("enable_dual_filter", 0); // =0
+    br->sym("enable_order_hint", 0); // =0
+    br->sym("enable_jnt_comp", 0); // =0
+    br->sym("enable_ref_frame_mvs", 0); // =0
+    br->sym("seq_force_screen_content_tools", 0); // 0, should be SELECT_SCREEN_CONTENT_TOOLS(2)
+    br->sym("seq_force_integer_mv", 0); // 0, should be SELECT_INTEGER_MV(2)
+    br->sym("OrderHintBits", 0); // =0
+  }// else { // incomplete parsing
+
+  br->sym("enable_superres", 1);
+  br->sym("enable_cdef", 1);
+  br->sym("enable_restoration", 1);
+  auto colorConfigBits = parseAv1ColorConfig(br, av1c.seq_profile, av1c);
+  br->sym("film_grain_params_present", 1);
+
+  auto readBits = 17 + (frame_width_bits_minus_1 + 1) + (frame_height_bits_minus_1 + 1) + 3 + 3 + colorConfigBits + 1;
+  READ_UNTIL_NEXT_BYTE(readBits);
+  return readBits / 8;
 }
 
 int parseAv1UncompressedHeader(IReader* br, const bool reduced_still_picture_header)
@@ -49,7 +242,7 @@ enum
   OBU_REDUNDANT_FRAME_HEADER = 7,
 };
 
-void parseAv1Obus(IReader* br, bool& reduced_still_picture_header)
+void parseAv1Obus(IReader* br, bool& reduced_still_picture_header, AV1CodecConfigurationRecord& av1c)
 {
   br->sym("obu", 0); // virtual OBU separator
   br->sym("forbidden", 1);
@@ -95,7 +288,7 @@ void parseAv1Obus(IReader* br, bool& reduced_still_picture_header)
   {
   case OBU_SEQUENCE_HEADER:
     br->sym("seqhdr", 0);
-    obuSize -= parseAv1SeqHdr(br, reduced_still_picture_header);
+    obuSize -= parseAv1SeqHdr(br, reduced_still_picture_header, av1c);
     break;
   case OBU_FRAME_HEADER: case OBU_REDUNDANT_FRAME_HEADER: case OBU_FRAME:
     br->sym("frmhdr", 0);
@@ -141,8 +334,9 @@ void parseAv1C(IReader* br)
 
   while(!br->empty())
   {
+    AV1CodecConfigurationRecord av1c {};
     bool reduced_still_picture_header = false;
-    parseAv1Obus(br, reduced_still_picture_header);
+    parseAv1Obus(br, reduced_still_picture_header, av1c);
   }
 }
 
@@ -400,10 +594,11 @@ static const SpecDesc spec =
           BoxReader br;
           br.br = BitReader { bytes.data(), (int)bytes.size() };
 
-          bool reduced_still_picture_header = false;
+          bool reduced_still_picture_header_unused = false;
+          AV1CodecConfigurationRecord av1c_unused {};
 
           while(!br.empty())
-            parseAv1Obus(&br, reduced_still_picture_header);
+            parseAv1Obus(&br, reduced_still_picture_header_unused, av1c_unused);
 
           /* we know we've seen the sequence header (av1C) and the frame header*/
           bool showFrame = false, keyFrame = false;
@@ -439,10 +634,11 @@ static const SpecDesc spec =
           BoxReader br;
           br.br = BitReader { bytes.data(), (int)bytes.size() };
 
-          bool reduced_still_picture_header = false;
+          bool reduced_still_picture_header_unused = false;
+          AV1CodecConfigurationRecord av1c_unused {};
 
           while(!br.empty())
-            parseAv1Obus(&br, reduced_still_picture_header);
+            parseAv1Obus(&br, reduced_still_picture_header_unused, av1c_unused);
 
           assert(br.myBox.children.empty());
 
@@ -469,10 +665,11 @@ static const SpecDesc spec =
           BoxReader br;
           br.br = BitReader { bytes.data(), (int)bytes.size() };
 
-          bool reduced_still_picture_header = false;
+          bool reduced_still_picture_header_unused = false;
+          AV1CodecConfigurationRecord av1c_unused {};
 
           while(!br.empty())
-            parseAv1Obus(&br, reduced_still_picture_header);
+            parseAv1Obus(&br, reduced_still_picture_header_unused, av1c_unused);
 
           assert(br.myBox.children.empty());
 
@@ -498,9 +695,10 @@ static const SpecDesc spec =
           br.br = BitReader { bytes.data(), (int)bytes.size() };
 
           bool reduced_still_picture_header = false;
+          AV1CodecConfigurationRecord av1c_unused {};
 
           while(!br.empty())
-            parseAv1Obus(&br, reduced_still_picture_header);
+            parseAv1Obus(&br, reduced_still_picture_header, av1c_unused);
 
           if(!reduced_still_picture_header)
             out->error("reduced_still_picture_header flag set to 0.");
@@ -523,8 +721,70 @@ static const SpecDesc spec =
       "Sequence Header OBU in the AV1 Image Item Data.",
       [] (Box const& root, IReport* out)
       {
-        (void)root;
-        (void)out; // TODO
+        AV1CodecConfigurationRecord av1cRef {};
+
+        for(auto& box : root.children)
+        {
+          if(box.fourcc == FOURCC("meta"))
+          {
+            auto av1Cs = findBoxes(box, FOURCC("av1C"));
+
+            if(av1Cs.size() != 1)
+              out->warning("Several av1C found. Please contact us to provide a sample!");
+
+            for(auto& av1C : av1Cs)
+            {
+              for(auto& sym : av1C->syms)
+              {
+                if(!strcmp(sym.name, "seq_profile"))
+                  av1cRef.seq_profile = sym.value;
+
+                if(!strcmp(sym.name, "seq_level_idx_0"))
+                  av1cRef.seq_level_idx_0 = sym.value;
+
+                if(!strcmp(sym.name, "seq_tier_0"))
+                  av1cRef.seq_tier_0 = sym.value;
+
+                if(!strcmp(sym.name, "high_bitdepth"))
+                  av1cRef.high_bitdepth = sym.value;
+
+                if(!strcmp(sym.name, "twelve_bit"))
+                  av1cRef.twelve_bit = sym.value;
+
+                if(!strcmp(sym.name, "monochrome"))
+                  av1cRef.mono_chrome = sym.value;
+
+                if(!strcmp(sym.name, "chroma_subsampling_x"))
+                  av1cRef.chroma_subsampling_x = sym.value;
+
+                if(!strcmp(sym.name, "chroma_subsampling_y"))
+                  av1cRef.chroma_subsampling_y = sym.value;
+
+                if(!strcmp(sym.name, "chroma_sample_position"))
+                  av1cRef.chroma_sample_position = sym.value;
+              }
+            }
+          }
+        }
+
+        auto const av1ImageItemIDs = findAv1ImageItems(root);
+
+        for(auto itemId : av1ImageItemIDs)
+        {
+          auto bytes = getAV1ImageItemData(root, out, itemId);
+
+          BoxReader br;
+          br.br = BitReader { bytes.data(), (int)bytes.size() };
+
+          bool reduced_still_picture_header_unused = false;
+          AV1CodecConfigurationRecord av1c {};
+
+          while(!br.empty())
+            parseAv1Obus(&br, reduced_still_picture_header_unused, av1c);
+
+          if(memcmp(&av1c, &av1cRef, sizeof(AV1CodecConfigurationRecord)))
+            out->error("The values of the AV1CodecConfigurationBox shall match the Sequence Header OBU in the AV1 Image Item Data.");
+        }
       }
     },
     {
@@ -564,10 +824,11 @@ static const SpecDesc spec =
           BoxReader br;
           br.br = BitReader { bytes.data(), (int)bytes.size() };
 
-          bool reduced_still_picture_header = false;
+          bool reduced_still_picture_header_unused = false;
+          AV1CodecConfigurationRecord av1c_unused {};
 
           while(!br.empty())
-            parseAv1Obus(&br, reduced_still_picture_header);
+            parseAv1Obus(&br, reduced_still_picture_header_unused, av1c_unused);
 
           assert(br.myBox.children.empty());
 
