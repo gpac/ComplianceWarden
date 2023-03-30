549564d649e6ea7dd13ed60dbf795aa01edf2eae
Dimitri Podborski <dpodborski@apple.com>
2023-03-30 08:53:03 -0700

    fetch html with asserts from the source

--- scripts/hdr10plusrules.py
+++ scripts/hdr10plusrules.py
@@ -1,13 +1,16 @@
 import argparse
+import urllib.request
 
 from bs4 import BeautifulSoup
+
+SPEC_URL = "https://aomediacodec.github.io/av1-hdr10plus/"
 
 src_file = "../src/specs/av1_hdr10plus/av1_hdr10plus.cpp"
 
 parser = argparse.ArgumentParser(
     description="Check if all rules from the HDR10+ spec are implemented.")
 
-parser.add_argument("-i", "--input", help="Spec HTML file.", required=True)
+parser.add_argument("-i", "--input", help="Spec HTML file.")
 parser.add_argument(
     "--dump", help="Dump code snippet based on asserts in HTML.", action="store_true")
 args = parser.parse_args()
@@ -15,8 +18,12 @@ args = parser.parse_args()
 with open(src_file, "r", encoding="utf-8") as src_f:
     src = src_f.read()
 
-with open(args.input, "r", encoding="utf-8") as f:
-    soup = BeautifulSoup(f, features='lxml')
+if args.input is not None:
+    with open(args.input, "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f, features='lxml')
+else:
+    html_string = urllib.request.urlopen(SPEC_URL).read()
+    soup = BeautifulSoup(html_string, features='lxml')
 
 assert_spans = soup.find_all(
     "span", {"id": lambda L: L and L.startswith("assert-")})
