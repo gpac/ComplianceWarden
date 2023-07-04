8a19b7edb0a3f1c62739246cfd482fe0358ca92a
Phencys <phencys@phencys.com>
2023-07-04 14:34:54 +0200

    Update description for codec configuration box

--- src/specs/av1_isobmff/av1_isobmff.cpp
+++ src/specs/av1_isobmff/av1_isobmff.cpp
@@ -263,9 +263,8 @@ namespace {
              out->covered();
            }},
           {"Section 2.2.4\n"
-           "Additionally, if MaxRenderWidth and MaxRenderHeight values do not equal respectively "
-           "the max_frame_width_minus_1 + 1 and max_frame_height_minus_1 + 1 values of the "
-           "Sequence Header OBU, a PixelAspectRatioBox box SHALL be present in the sample entry",
+           "The config field SHALL contain an AV1CodecConfigurationBox that applies to the samples "
+           "associated with this sample entry.",
            [](Box const &root, IReport *out) {
              auto obuDetails = getOBUDetails(root, out);
              if (!obuDetails.valid) { return; }
