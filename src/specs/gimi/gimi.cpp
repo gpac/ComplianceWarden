e225d06d141d6638ce831b0498cf9a81d6cadee0
dukesook <devonsookhoo14@gmail.com>
2025-01-22 13:14:09 -0700

    gimi: 'use' root & out variables to temporarily bypass warnings

--- src/specs/gimi/gimi.cpp
+++ src/specs/gimi/gimi.cpp
@@ -52,6 +52,8 @@ static const SpecDesc specGimi = {
     { "Requirement NGA.STND.0076_1.0-04\n"
       "An NGA.STND.0076 file shall include the 'unif' brand in the compatible brands list.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: The 'unif' brand indicates the unified implementation and handling of
         IDs across file-scoped MetaBox items, tracks, track groups, and entity groups.
         */
@@ -61,6 +63,8 @@ static const SpecDesc specGimi = {
       "Where an NGA.STND.0076 file contains Still Imagery content, the file shall conform to the 'mif2' brand "
       "requirements.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: The ‘mif2’ brand represents interoperability requirements for image and metadata items. ‘mif2’
          * represents a baseline for Still Imagery support in this standard. The HEIF standard documents the specifics
          * of the branding differences.*/
@@ -69,6 +73,8 @@ static const SpecDesc specGimi = {
     { "Requirement NGA.STND.0076_1.0-06\n"
       "Where an NGA.STND.0076 file contains Still Imagery content, the file shall include 'mif2' brand.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: */
       } },
 
@@ -76,12 +82,16 @@ static const SpecDesc specGimi = {
       "Where an NGA.STND.0076 file contains image sequence content, the file shall conform to the requirements "
       "associated with the 'msf1' brand.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: The 'msf1' brand indicates the presence of a HEIF defined image sequence.*/
       } },
 
     { "Requirement NGA.STND.0076_1.0-08\n"
       "Where an NGA.STND.0076 file contains image sequence content, the file shall include the 'msf1' brand.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: */
       } },
 
@@ -89,6 +99,8 @@ static const SpecDesc specGimi = {
       "Where an NGA.STND.0076 file contains Motion Imagery content, the file shall conform to the requirements "
       "associated with the 'isoa' brand.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: The ‘isoa’ brand represents interoperability requirements for the base format as well as Motion Imagery
          * requirements for this standard. */
       } },
@@ -97,8 +109,11 @@ static const SpecDesc specGimi = {
       "NGA.STND.0076_1.0-10	Where an NGA.STND.0076 file contains Motion Imagery content, the file shall include "
       "the 'isoa' brand.",
       [](Box const &root, IReport *out) {
+        (void)root;
+        (void)out;
         /* TODO: */
       } },
+
   },
   isIsobmff,
 };
